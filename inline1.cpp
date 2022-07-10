a3e417b8a11d247e3c2874b1cd7c8e03f284adae
sandeep ghanwat <101173367+ssghanwat@users.noreply.github.com>
2022-07-11 00:15:47 +0530

    Add files via upload

---
+++ inline1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+	public:
+		inline int Add(int no1,int no2)
+		{
+			int ans=0;
+			ans=no1 + no2;
+			return ans;
+		}
+};
+int main()
+{
+	Demo obj;
+	int ret=0;
+	cout<<"\nSize"<<sizeof(obj);
+	
+	ret=obj.Add(10,11);
+	
+	cout<<"\n"<<ret;
+	
+	return 0;
+}
